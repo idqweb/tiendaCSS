44b9010050dad1b57ab816a12fcb1f8a15121b7b
idqweb <isaac@idqweb.com>
2015-12-08 14:42:43 +0100

    Camiseta colores

---
+++ camisetaCambiacolor/camiseta.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Portada - Tienda Deportes</title>
+		
+		<!-- Etiqueta necesaria para ser Responsive HTML5 -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+		
+		<!-- Los estilos css de la página -->
+			<link rel="stylesheet" href="cambiancolor.css"/>
+		
+	</head>
+	<body>
+<article>
+	<input type="radio" name="color" id="azul"/>
+  <input type="radio" name="color" id="lila"/>
+	<input type="radio" name="color" id="deepV"/>
+	<input type="radio" name="color" id="oliva"/>
+	<input type="radio" name="color" id="rosa"/>
+	<input type="radio" name="color" id="naranja"/>
+	<input type="radio" name="color" id="gris"/>
+  
+  <input type="radio" id="camiseta" name="prenda" checked />
+	<input type="radio" id="pantalon" name="prenda" />
+	<input type="radio" id="zapatilla" name="prenda" />
+  <nav class='colores'>
+	<label for="azul"></label>
+	<label for="deepV"></label>
+	<label for="oliva"></label>
+	<label for="lila"></label>
+	<label for="rosa"></label>
+	<label for="naranja"></label>
+	<label for="gris"></label>
+</nav>
+<nav class='prendas'>
+	<label for="camiseta"></label>
+	<label for="pantalon"></label>
+	<label for="zapatilla"></label>
+</nav>
+	<img class='camiseta' src="./images/camisetabilbao.jpg" alt="Camiseta" />
+	<img class='pantalon' src="http://4.bp.blogspot.com/-V-a54eWqRIE/U77YrCA5UvI/AAAAAAAAJno/Zw621wW_5_4/s1600/pantalon.jpg" alt="Pantalon" />
+	<img class='zapatilla' src="http://1.bp.blogspot.com/-Excr4mp1gEU/U77gup9B_8I/AAAAAAAAJn8/P3zIj3DU9ig/s1600/zapatillas.jpg" alt="zapatilla" />
+</article>
+
+<p>If you need a post about the pen, <a href='http://ksesocss.blogspot.com/2014/07/cien-tonos-color-una-imagen-css.html'>go to my blog</a>. There is some credits.</p> 
+
+		
+	</body>
+</html>
+		
+		
